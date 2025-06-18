d0f18eae2a61d4e094ba7d9f5b3f98d0d114dd03
CosmicCooter <oscarlau.734@gmail.com>
2025-06-17 23:21:55 -0500

    comit init mas usado

--- GestionlentesvrBack/GestionlentesvrPersistencia/src/main/java/com/pucp/gestionlentesvr/persistencia/dao/usuario/MetricaUsoDAO.java
+++ GestionlentesvrBack/GestionlentesvrPersistencia/src/main/java/com/pucp/gestionlentesvr/persistencia/dao/usuario/MetricaUsoDAO.java
@@ -1,8 +1,13 @@
 package com.pucp.gestionlentesvr.persistencia.dao.usuario;
 
+import com.pucp.gestionlentesvr.dominio.aplicacion.Aplicacion;
+import com.pucp.gestionlentesvr.dominio.dispositivo.Dispositivo;
 import com.pucp.gestionlentesvr.dominio.usuario.MetricaUso;
 import com.pucp.gestionlentesvr.persistencia.BaseDAO;
+import java.util.List;
 
 public interface MetricaUsoDAO extends BaseDAO<MetricaUso> {
-    
+    public Aplicacion obtenerAppMasUsada();
+    public Dispositivo obtenerDispositivoMasUsado();
+    public Dispositivo obtenerDispositivoMenosUsado();
 }
